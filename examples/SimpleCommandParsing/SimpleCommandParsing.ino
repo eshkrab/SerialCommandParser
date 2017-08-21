aa36dada07d611c08222d624860e5615781b3080
Rina Shkrabova <eshkrab@gmail.com>
2017-08-21 12:44:58 -0700

    added a simple example

---
+++ examples/SimpleCommandParsing/SimpleCommandParsing.ino
@@ -0,0 +1,59 @@
+//Parser expects commands to be separated by semicolons (;) and will ignore input that is not terminated this way
+//To test, send commands like "p;" or "s;"
+#include "SerialCmdParser.h"
+#define LED 13
+
+//Init the parser
+SerialCmdParser parser = SerialCmdParser();
+//Specify which commands to listen for
+char cmds[] = {'t','p','s'};
+
+void dataHandler(char cmd, int idx, void * data){
+  //print our incoming command data
+  Serial.print("cmd: ");
+  Serial.println(cmd);
+  Serial.print("idx: ");
+  Serial.println(idx);
+  if (data != NULL) {
+    Serial.print("data: ");
+    Serial.println((char *)data);
+  }
+
+  //let's do something with this data
+  switch (cmd) {
+    case 't':
+      Serial.println(millis());
+      break;
+    case 'p':
+      Serial.println("PLAY:");
+      digitalWrite(LED,HIGH);
+      break;
+    case 's':
+      Serial.println("STOP:");
+      digitalWrite(LED,LOW);
+      break;
+  }
+
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000); //waiting for Serial to init, for teensy
+
+  parser.setCmds(cmds, sizeof(cmds));
+  parser.setCallback(dataHandler);
+
+  Serial.println("Parser handles the following commands: (don't forget ';')");
+  for (int i=0; i<sizeof(cmds); i++) {
+    Serial.print(cmds[i]);
+    Serial.println(";");
+  }
+
+  pinMode(13, OUTPUT);
+  
+}
+
+void loop() {
+  parser.update();
+  
+}
